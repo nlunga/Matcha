c000391d1f1fd478bf80cc0224a6a7dc62126870
Nqabutho LUNGA <nlunga@student.wethinkcode.co.za>
2020-01-02 11:07:15 +0200

    fix the partials

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// use res.render to load up an ejs view file
+
 
 // app.get('/', (req, res) => res.send('Hello World!'))
+// index page 
 app.get("/", (req, res) => {
-    res.send("test.html");
+    res.render("index");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+ 
