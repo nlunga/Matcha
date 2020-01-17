20cf054a4c96f76e5452dbabac868e8861198a5d
Orion DOOMS <odooms@c5r4s10.wethinkcode.co.za>
2020-01-17 14:09:51 +0200

    stuff

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const joi = require('joi');
 // const mongo = require('mongodb');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 //////////////////////////////////////////////////
 /// DATABASE CREATITION
